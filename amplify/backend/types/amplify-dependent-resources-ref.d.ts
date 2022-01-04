eb8e86288e8c9a4cc9bbcdf6bd29aef7800ec378
Callen <naporaradekbhd@gmail.com>
2022-01-04 01:36:02 +0100

    build: add AWS amplify
    * add amplify api
    * add amplify auth
    * setup todo schema

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "eisenhowermatrixampl": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "eisenhowermatrixampl5925eef3": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
