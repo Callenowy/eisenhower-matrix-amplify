33d291895ecb7430ccd01c4831cc8c91f9869d61
Callen <naporaradekbhd@gmail.com>
2022-01-09 23:28:23 +0100

    chore(next.config.js): add security headers

--- next.config.js
+++ next.config.js
@@ -1,4 +1,50 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  poweredByHeader: false,
+  swcMinify: false, // disabled till app use babel
+
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on',
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'Permissions-Policy',
+            value:
+              'camera=(), microphone=(), geolocation=(), interest-cohort=()',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+          /* @TODO: ADD CSP */
+          // {
+          //   key: 'Content-Security-Policy',
+          //   value: // Your CSP Policy
+          // }
+        ],
+      },
+    ];
+  },
 };
